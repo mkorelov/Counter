17495c40ad3017aee9aa3730ee65d6aa30130dd6
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-09-10 16:37:07 -0700

    added and formated buttons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,62 @@
 <template>
+
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+
+    <div class="row full-width items-center">
+
+      <div class="col-3 text-right">
+
+        <q-btn
+
+          size="35px"
+          round
+          color="red-14"
+          icon="mic"
+
+        />
+
+      </div>
+
+      <div class="col-6 text-center">
+
+        <audio controls>
+
+
+
+        </audio>
+
+      </div>
+
+      <div class="col-3 text-left">
+
+       <q-btn
+
+          size="35px"
+          round
+          color="red-14"
+          icon="front_hand"
+
+        />
+
+      </div>
+
+    </div>
+
   </q-page>
+
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+
+<script>
+
+// TODO
+
 </script>
+
+
+<style scoped>
+  .q-page {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+</style>
