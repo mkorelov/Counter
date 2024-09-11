c8eb5b1613d5337948ba3e47ca2d9992ffd5f9ef
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-09-10 17:47:34 -0700

    implemented buttons and script

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -12,6 +12,7 @@
           round
           color="red-14"
           icon="mic"
+          id="startButton"
 
         />
 
@@ -19,11 +20,7 @@
 
       <div class="col-6 text-center">
 
-        <audio controls>
-
-
-
-        </audio>
+        <audio controls id="audioControls"></audio>
 
       </div>
 
@@ -35,6 +32,7 @@
           round
           color="red-14"
           icon="front_hand"
+          id="stopButton"
 
         />
 
@@ -49,14 +47,60 @@
 
 <script>
 
-// TODO
+  let audioIn = { audio: true };
+
+    navigator.mediaDevices.getUserMedia(audioIn)
+
+      .then(function (mediaStreamObj) {
+
+        let start = document.getElementById('startButton');
+
+        let stop = document.getElementById('stopButton');
+
+        let playAudio = document.getElementById('audioControls');
+
+        let mediaRecorder = new MediaRecorder(mediaStreamObj);
+
+        start.addEventListener('click', function (ev) {
+          mediaRecorder.start();
+        })
+
+        stop.addEventListener('click', function (ev) {
+          mediaRecorder.stop();
+        });
+
+        mediaRecorder.ondataavailable = function (ev) {
+          dataArray.push(ev.data);
+        }
+
+        let dataArray = [];
+
+        mediaRecorder.onstop = function (ev) {
+
+          let audioData = new Blob(dataArray,
+                    { 'type': 'audio/mp3;' });
+
+          dataArray = [];
+
+          let audioSrc = window.URL
+              .createObjectURL(audioData);
+
+          playAudio.src = audioSrc;
+        }
+      })
+
+      .catch(function (err) {
+        console.log(err.name, err.message);
+      });
 
 </script>
 
 
 <style scoped>
+
   .q-page {
     max-width: 700px;
     margin: 0 auto;
   }
+
 </style>
